c09621f2422b354175dc6ba6abde805419410bf4
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2022-03-04 08:12:01 -0500

    working on social club page

--- pages/jackies.js
+++ pages/jackies.js
@@ -20,22 +20,29 @@ function jackies() {
           className="absolute inset-0 bg-gray-900 opacity-50"
         />
 
-        <div className="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0">
+        <div className="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-72 lg:px-0">
           <h1 className="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">
-            The New Classic
+            A Playground for the Patrons
           </h1>
-          <p className="mt-4 text-xl text-white">
-            With a tip of the hat to New York style, Herald Towers, previously
-            the famous Hotel McAlpin, has been restored and reimagined, fusing
-            accents of old classic grandeur with immaculate, innovative
-            residences.
-          </p>
-          <a
-            href="#"
-            className="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100"
-          >
-            View Availability
-          </a>
+        </div>
+      </div>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+              Social Room
+            </h2>
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Jackie's Club
+            </p>
+            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+              Through ever-unfolding intimate alcoves, and work nooks, the games
+              room and bar lounge on the 25th floor open out onto the grandeur
+              of the Main Room. Under an enormous wagon wheel chandelier, the
+              seductive space invites social reprieve, featuring velvet modular
+              seating, a large flatscreen TV, and lavish space to entertain.
+            </p>
+          </div>
         </div>
       </div>
     </>
