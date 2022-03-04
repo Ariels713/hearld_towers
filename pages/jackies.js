94f18f54502b2b7d408833521681ade92e63d712
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2022-03-04 08:33:00 -0500

    finished jackies social room

--- pages/jackies.js
+++ pages/jackies.js
@@ -1,25 +1,19 @@
 import React from "react"
 import Image from "next/image"
-import hearldMenu from "../public/assets/hearldMenu.jpeg"
+import jackies1 from "../public/assets/jackies/jackies1.jpg"
+import jackies2 from "../public/assets/jackies/jackies2.jpg"
+import jackies3 from "../public/assets/jackies/jackies3.jpg"
+import jackies4 from "../public/assets/jackies/jackies4.jpg"
 
-const images = [
+const faqs = [
   {
-    src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
-    alt: "Two each of gray, white, and black shirts laying flat.",
+    question: "Get comfortable and stay awhile",
+    answer:
+      "Through ever-unfolding intimate alcoves, and work nooks, the games room and bar lounge on the 25th floor open out onto the grandeur of the Main Room. Under an enormous wagon wheel chandelier, the seductive space invites social reprieve, featuring velvet modular seating, a large flatscreen TV, and lavish space to entertain. ",
   },
-  {
-    src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
-    alt: "Model wearing plain black basic tee.",
-  },
-  {
-    src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
-    alt: "Model wearing plain gray basic tee.",
-  },
-  {
-    src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
-    alt: "Model wearing plain white basic tee.",
-  },
+  // More questions...
 ]
+
 function jackies() {
   return (
     <>
@@ -28,14 +22,14 @@ function jackies() {
         <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
           <Image
             className="w-full h-full object-center object-cover"
-            src={hearldMenu}
+            src={jackies1}
             alt="Launge Room at Hearld"
             layout="fill"
           />
         </div>
         <div
           aria-hidden="true"
-          className="absolute inset-0 bg-gray-900 opacity-50"
+          className="absolute inset-0 bg-gray-900 opacity-25"
         />
 
         <div className="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-72 lg:px-0">
@@ -65,34 +59,67 @@ function jackies() {
       </div>
       <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
         <div className="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
-          <img
-            src={images[0].src}
-            alt={images[0].alt}
-            className="w-full h-full object-center object-cover"
+          <Image
+            src={jackies2}
+            layout="fill"
+            alt=""
+            className="w-full h-full object-center object-cover aspect-w-3 aspect-h-4"
           />
         </div>
         <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
           <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-            <img
-              src={images[1].src}
-              alt={images[1].alt}
+            <Image
+              layout="fill"
+              src={jackies3}
+              alt=""
               className="w-full h-full object-center object-cover"
             />
           </div>
           <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-            <img
-              src={images[2].src}
-              alt={images[2].alt}
+            <Image
+              layout="fill"
+              src={jackies1}
+              alt=""
               className="w-full h-full object-center object-cover"
             />
           </div>
         </div>
         <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-          <img
-            src={images[3].src}
-            alt={images[3].alt}
+          <Image
+            layout="fill"
+            src={jackies4}
+            alt=""
             className="w-full h-full object-center object-cover"
           />
+        </div>
+      </div>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+            <div>
+              <h2 className="text-3xl font-extrabold text-gray-900">
+                Jackie's Social Room
+              </h2>
+              <p className="mt-4 text-lg text-gray-500">
+                Quick Fact: When Hotel McAlpin opened it was the largest hotel
+                in the world.
+              </p>
+            </div>
+            <div className="mt-12 lg:mt-0 lg:col-span-2">
+              <dl className="space-y-12">
+                {faqs.map((faq) => (
+                  <div key={faq.question}>
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      {faq.question}
+                    </dt>
+                    <dd className="mt-2 text-base text-gray-500">
+                      {faq.answer}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
         </div>
       </div>
     </>
